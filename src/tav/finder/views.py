bad2c13b3a99500e844b49fc32c8b565f345edab
Akiva Bloch <akiva.bloch@bluevine.com>
2015-02-17 22:34:19 +0200

    add some orm to view

--- src/tav/finder/views.py
+++ src/tav/finder/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from finder.models import Business
 # Create your views here.
 def index(request):
-	b=Business.all()
-	return render(request,"finder/business.html")
+	b=Business.objects.all()
+	return render(request,"finder/business.html",{'business':b})
