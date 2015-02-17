c45582d0c941f1981dab74ae435e615adcf95ce4
Akiva Bloch <akiva.bloch@bluevine.com>
2015-02-17 22:30:36 +0200

    use livereload

--- src/tav/finder/views.py
+++ src/tav/finder/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from finder.models import Business
 # Create your views here.
 def index(request):
+	b=Business.all()
 	return render(request,"finder/business.html")
